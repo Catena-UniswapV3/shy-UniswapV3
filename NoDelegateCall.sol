836ad86e65c10018074c064f45d47e644e1d9d6e
shypang <smartdragon417@gmail.com>
2023-04-12 22:13:54 +0900

    Add: NoDelegateCall.sol and UniswapV3PoolDeployer.sol

--- NoDelegateCall.sol
+++ NoDelegateCall.sol
@@ -5,21 +5,27 @@ pragma solidity =0.7.6;
 /// @notice Base contract that provides a modifier for preventing delegatecall to methods in a child contract
 abstract contract NoDelegateCall {
     /// @dev The original address of this contract
+    //address 주소 변수 설정 
     address private immutable original;
 
+    //생성자에서 현재 주소를 original 변수로 설정 
     constructor() {
         // Immutables are computed in the init code of the contract, and then inlined into the deployed bytecode.
         // In other words, this variable won't change when it's checked at runtime.
         original = address(this);
     }
 
+    
     /// @dev Private method is used instead of inlining into modifier because modifiers are copied into each method,
     ///     and the use of immutable means the address bytes are copied in every place the modifier is used.
+    /// modifier가 각 메서드에 복사되기 때문에 modifier에 inlining하는 대신 Private 메서드가 사용됩니다,
+    /// 그리고 immutable을 사용하면 modifier가 사용되는 모든 위치에 주소 바이트가 복사됩니다.
     function checkNotDelegateCall() private view {
         require(address(this) == original);
     }
 
     /// @notice Prevents delegatecall into the modified method
+    //수정된 메서드에 대한 delegatecall 호출 방지
     modifier noDelegateCall() {
         checkNotDelegateCall();
         _;
--- UniswapV3PoolDeployer.sol
+++ UniswapV3PoolDeployer.sol
@@ -7,23 +7,26 @@ import './UniswapV3Pool.sol';
 
 contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
     struct Parameters {
-        address factory;
-        address token0;
-        address token1;
-        uint24 fee;
-        int24 tickSpacing;
+        address factory; //팩토리 주소 
+        address token0;  //토큰0 주소 
+        address token1;  //토큰1 주소 
+        uint24 fee;      //수수료 
+        int24 tickSpacing; //집중유동성 구간과 유동성 구간이 아닌 곳을 구분하는 변수 
     }
 
     /// @inheritdoc IUniswapV3PoolDeployer
+    // IUniswapV3PoolDeployer 상속했다는 것인가? 
     Parameters public override parameters;
 
     /// @dev Deploys a pool with the given parameters by transiently setting the parameters storage slot and then
     /// clearing it after deploying the pool.
-    /// @param factory The contract address of the Uniswap V3 factory
-    /// @param token0 The first token of the pool by address sort order
-    /// @param token1 The second token of the pool by address sort order
-    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// @param tickSpacing The spacing between usable ticks
+    // 파라미터 저장 슬롯을 일시적으로 설정하여 주어진 파라미터가 포함된 풀을 배포한 다음
+    /// 풀을 배포한 후 지웁니다.
+    /// @param factory The contract address of the Uniswap V3 factory @param factory 유니스왑 V3 팩토리의 컨트랙트 주소입니다.
+    /// @param token0 The first token of the pool by address sort  주소 정렬 순서에 따른 풀의 첫 번째 토큰입니다.
+    /// @param token1 The second token of the pool by address sort order   주소 정렬 순서에 따른 풀의 두 번째 토큰
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip 풀에서 스왑할 때마다 징수하는 수수료로, 100분의 1 bip 단위로 표시합니다. bip란 무엇인가?? 
+    /// @param tickSpacing The spacing between usable ticks 사용 가능한 틱 사이의 간격, 집중유동성구간과 아닌 구간 사이의 간격을 뜻하는 것인가? 아니면 집중유동성 구간을 뜻하는 것인가? 
     function deploy(
         address factory,
         address token0,
@@ -31,8 +34,8 @@ contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
         uint24 fee,
         int24 tickSpacing
     ) internal returns (address pool) {
-        parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
-        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}());
-        delete parameters;
+        parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing}); //파라미터 1:1 대응? 
+        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}()); //풀 변수에 address 함수 값을 넣고 이 안에 유니스왑v3 pool 주소를 넣는 것으로 보인다. 
+        delete parameters; //위에 설정한 파라미터를 삭제하는 것으로 보인다. 
     }
 }
